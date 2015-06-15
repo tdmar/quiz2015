c5c4e4948ecc9f62c524c769b28c021f44dc1113
tdmar <mariadelmar.trapagaduarte@telefonica.com>
2015-06-15 16:10:50 +0200

    'Primera_pregunta'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+/*MTD Se añade un nuevo controlador*/
+var quizController = require ('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
+
+/*MTD Introducir dos nuevas vistas */
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
